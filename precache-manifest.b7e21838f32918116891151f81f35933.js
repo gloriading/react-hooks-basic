a5dafb9525954a027d91f42d98bb5418325d7b9a
gloriading <gloria.ding@broadridge.com>
2020-02-19 08:03:45 -0800

    Updates

---
+++ precache-manifest.b7e21838f32918116891151f81f35933.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce16e7e8f6578f1fc16f46382c5ff3a",
+    "url": "/react-hooks-basic/index.html"
+  },
+  {
+    "revision": "849e2813d7628ad1982a",
+    "url": "/react-hooks-basic/static/css/main.f4609e3d.chunk.css"
+  },
+  {
+    "revision": "6026b1439eb07e8a7b2f",
+    "url": "/react-hooks-basic/static/js/2.13e03167.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-hooks-basic/static/js/2.13e03167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849e2813d7628ad1982a",
+    "url": "/react-hooks-basic/static/js/main.c11bbc13.chunk.js"
+  },
+  {
+    "revision": "95996cd983e38ef4ac88",
+    "url": "/react-hooks-basic/static/js/runtime-main.f257164f.js"
+  }
+]);
